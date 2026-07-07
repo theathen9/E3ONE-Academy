8ef469529d08e872248e19c43d70185777dde872
theathen9 <theathen36@gmail.com>
2026-07-07 12:42:39 +0700

    Fix BASE_URL definition by removing leading slash

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 // ./config/app
-define('BASE_URL', '/https://system-academy.onrender.com');
+define('BASE_URL', 'system-academy.onrender.com');
 define('APP_SECRET', 'SECRET_KEY');
 define('BASE_INSTITUTE', '/system-management/admin/institute');
 define('BASE_SIS', '/system-management/admin/sis');
