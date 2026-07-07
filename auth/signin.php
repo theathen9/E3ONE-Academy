f0541b48f9b4a02504b493fa2931390cbe2c34ff
theathen9 <theathen36@gmail.com>
2026-07-07 23:20:34 +0700

    Uncomment auth.php inclusion in signin.php

--- auth/signin.php
+++ auth/signin.php
@@ -2,7 +2,7 @@
 // ./auth/signin.php (from Page)
 date_default_timezone_set('Asia/Phnom_Penh');
 include_once __DIR__ . '/../config/bootstrap.php';
-// include_once __DIR__ . '/../app/api/v1/auth.php';
+include_once __DIR__ . '/../app/api/v1/auth.php';
 $user = checkAuth();
 if ($user && isset($_SESSION['loggedin'])) {
     switch (strtolower($_SESSION['role'])) {
