70c0b1fc1cbcb24555cb7ecb3ab1d06a185302c1
theathen9 <theathen36@gmail.com>
2026-07-07 23:40:46 +0700

    Refactor bootstrap.php for session and require statements
    Removed duplicate session_start() call and adjusted require statements for better organization.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-session_start();
 // ./config/bootstrap.php
+
+session_start();
+
 date_default_timezone_set('Asia/Phnom_Penh');
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -13,10 +15,8 @@ if (file_exists($envPath . '/.env')) {
     Dotenv::createImmutable($envPath)->load();
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/app.php';
-
 
 require_once __DIR__ . '/../helpers/request.php';
 require_once __DIR__ . '/../helpers/response.php';
@@ -25,5 +25,4 @@ require_once __DIR__ . '/../helpers/csrf.php';
 
 require_once __DIR__ . '/../core/DB.php';
 require_once __DIR__ . '/../core/ORM.php';
-require_once __DIR__ . '/../app/api/v1/auth.php';
 require_once __DIR__ . '/../core/Cache.php';
