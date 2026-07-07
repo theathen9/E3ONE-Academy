86be753b8ee75eaa90a478dacb06c513c6043d84
theathen9 <theathen36@gmail.com>
2026-07-07 23:39:27 +0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,6 +15,8 @@ if (file_exists($envPath . '/.env')) {
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/app.php';
+
 
 require_once __DIR__ . '/../helpers/request.php';
 require_once __DIR__ . '/../helpers/response.php';
