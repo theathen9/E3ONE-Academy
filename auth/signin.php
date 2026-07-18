3c01b301db39852e9be24abf46ddf398df08af61
theathen9 <theathen36@gmail.com>
2026-07-18 23:31:14 +0700

    Fix require paths and update page title dynamically

--- auth/signin.php
+++ auth/signin.php
@@ -2,7 +2,8 @@
 // ./auth/signin.php (from Page)
 date_default_timezone_set('Asia/Phnom_Penh');
 
-require_once dirname(__DIR__) . '/config/bootstrap.php';
+require_once __DIR__ . '/../config/bootstrap.php';
+require_once __DIR__ . '/../data/dataSchema.php';
 require_once __DIR__ . '/../app/api/v1/auth.php';
 
 $user = checkAuth();
@@ -168,7 +169,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign in | Empowerment Education English One></title>
+    <title>Sign in | <?= htmlspecialchars($infoSchemaData[1]["name_short"]) ?></title>
     <link rel="icon" type="image/png" href="../src/assets/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
